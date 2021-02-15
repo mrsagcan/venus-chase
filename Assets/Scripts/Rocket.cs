d93767fd4ae422b9cc66d57c740a6f219248daee
Ahmet Emre Sağcan <mrsagcan@yahoo.com>
2021-02-15 17:02:50 +0300

    Added thrust source and did some adjustments in the scene

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    AudioSource thrustSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        thrustSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!thrustSound.isPlaying)
+                thrustSound.Play();
+        }
+
+        else
+        {
+            thrustSound.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.D))        
+            transform.Rotate(Vector3.back);
+
+        if (Input.GetKey(KeyCode.A))   
+            transform.Rotate(Vector3.forward);
+    }
+}
